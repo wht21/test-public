f3b9fedf1ee1285c0dbf5e8f2bea87261a5e74e3
luoxi <longyongqi.lyq@alibaba-inc.com>
2025-09-11 18:05:10 +0800

    Update get-CI-result.sh to correctly fetch CI status

--- script/get-CI-result.sh
+++ script/get-CI-result.sh
@@ -16,8 +16,7 @@ while true; do
 
     response=$(curl -s  -H "Content-Type: application/json" \
                         -H "Authorization: Basic ${SECURITY}" \
-                        -d "{\"commitId\": \"${COMMIT_ID}\"}"
-                        "https://get-tasend-back-twkvcdsbpj.cn-hangzhou.fcapp.run")
+                        -d "{\"commitId\": \"${COMMIT_ID}\"}" "https://get-tasend-back-twkvcdsbpj.cn-hangzhou.fcapp.run")
     status=$(echo "$response" | grep -o '"status":"[^"]*"' | cut -d':' -f2 | tr -d '"')
 
     echo "Current status: $status"
